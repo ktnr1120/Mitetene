70e857d0c6eaa02ce829679cb3dc5b53bc067236
ktnr1120 <ktnr1120@gmail.com>
2023-12-16 02:17:49 +0900

    投稿機能・保存完了

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use App\Models\Category;
 use App\Http\Requests\PostRequest;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
@@ -30,7 +31,30 @@ class PostController extends Controller
     
     public function store(PostRequest $request, Post $post)
     {
+            // 認証されているユーザーの情報を取得
+        $user = Auth::user();
+    
+        // デバッグ用：ログにユーザー情報を出力
+        \Log::info('Logged in user:', ['user' => $user]);
+    
+        // リクエストから投稿データを取得
         $input = $request['post'];
+        
+        $input += ['user_id' => $request->user()->id];
+    
+        // ログインしているユーザーのuser_idを投稿データに設定
+        $input['User_ID'] = $user->id;
+        
+        // ログインしているユーザーのChildren_IDを投稿データに設定
+        $input['Children_ID'] = $user->children_id;
+         
+         // Date カラムを現在の日付に設定
+        $input['Date'] = now();
+    
+        // デバッグ用：ログに投稿データを出力
+        \Log::info('Post data:', ['input' => $input]);
+    
+        // 投稿データを保存
         $post->fill($input)->save();
         return redirect('/posts/' . $post->id);
     }
@@ -43,6 +67,7 @@ class PostController extends Controller
     public function update(PostRequest $request, Post $post)
     {
         $input_post = $request['post'];
+        $input_post += ['user_id' => $request->user()->id];
         $post->fill($input_post)->save();
         
         return redirect('/posts/', $post->id);
--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    
+    //Post に対するリレーション
+    
+    //「1対多」の関係なので'posts'と複数形に
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
 }
