3e9e64b1a646ebb642f12f49c2d5f5dd8796b674
ktnr1120 <ktnr1120@gmail.com>
2024-02-28 00:01:05 +0900

    tailwindCSS導入

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,3 +4,9 @@ mix.react('resources/js/app.js', 'public/js')
    .postCss('resources/css/app.css', 'public/css', [
        //
    ]);
+
+// webpack.mix.js
+mix.postCss('resources/css/tailwind.css', 'public/css', [
+    require('tailwindcss'),
+]);
+
