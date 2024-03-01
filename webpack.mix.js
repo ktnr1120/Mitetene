1e98c3f9b362c77080de00338b1ebae079483ebb
ktnr1120 <ktnr1120@gmail.com>
2024-03-02 01:05:43 +0900

    tailwindビルド

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,12 +1,6 @@
 const mix = require('laravel-mix');
 
-mix.react('resources/js/app.js', 'public/js')
-   .postCss('resources/css/app.css', 'public/css', [
-       //
-   ]);
-
-// webpack.mix.js
-mix.postCss('resources/css/tailwind.css', 'public/css', [
+mix.postCss('resources/css/app.css', 'public/css', [
     require('tailwindcss'),
 ]);
 
