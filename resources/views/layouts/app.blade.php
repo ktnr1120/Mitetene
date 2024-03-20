831aacc4b3d67d4cc950edd17eca88303569c6ac
ktnr1120 <ktnr1120@gmail.com>
2024-03-21 00:21:39 +0900

    招待メールのフロント

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,11 +20,51 @@
 
             <!-- Page Heading -->
             @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <header class="text-gray-600 body-font">
+              <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+                <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+                    <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+                  </svg>
+                  <span class="ml-3 text-xl">
+                    <font style="vertical-align: inherit;">
+                      <font style="vertical-align: inherit;">みててねにようこそ</font>
+                    </font>
+                  </span>
+                </a>
+                <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
+                  <a class="mr-5 hover:text-gray-900">
+                    <font style="vertical-align: inherit;">
+                      <font style="vertical-align: inherit;">最初のリンク</font>
+                    </font>
+                  </a>
+                  <a class="mr-5 hover:text-gray-900">
+                    <font style="vertical-align: inherit;">
+                      <font style="vertical-align: inherit;">2 番目のリンク</font>
+                    </font>
+                  </a>
+                  <a class="mr-5 hover:text-gray-900">
+                    <font style="vertical-align: inherit;">
+                      <font style="vertical-align: inherit;">3番目のリンク</font>
+                    </font>
+                  </a>
+                  <a class="mr-5 hover:text-gray-900">
+                    <font style="vertical-align: inherit;">
+                      <font style="vertical-align: inherit;">4番目のリンク</font>
+                    </font>
+                  </a>
+                </nav>
+                <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+                  <font style="vertical-align: inherit;">
+                    <font style="vertical-align: inherit;">ボタン</font>
+                  </font>
+                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
+                    <path d="M5 12h14M12 5l7 7-7 7"></path>
+                  </svg>
+                </button>
+              </div>
+            </header>
                         {{ $header }}
-                    </div>
-                </header>
             @endif
 
             <!-- Page Content -->
